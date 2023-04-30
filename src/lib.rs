47773691c748934b8280d085e4a24943d39765fc
nvianin <n.vianin@gmail.com>
2023-04-30 17:57:24 +0200

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,14 @@ impl Nickname {
         name
     }
 
+    pub fn with_collision_avoidance(&self, names: &Vec<String>) -> String {
+        let mut name = self.name();
+        while names.contains(&name) {
+            name = self.name();
+        }
+        name
+    }
+
     fn random_chars(&self, number: u8) -> String {
         thread_rng()
             .sample_iter(&rand::distributions::Alphanumeric)
@@ -57,11 +65,27 @@ pub mod tests {
     use super::Nickname;
     use std::collections::HashMap;
     use std::thread;
+    use std::time::Instant;
 
     pub const INSERTS_TO_TRY: usize = 100_000_000;
     #[test]
-    pub fn median_nickname_collision_test(){
-        let results = Vec<usize>
+    pub fn median_nickname_collision_test() {
+        let mut results = Vec::new();
+        println!("Running 10 tests");
+        let start = Instant::now();
+        for _ in 0..10 {
+            let start = Instant::now();
+            results.push(nickname_test());
+            println!("Test took {}ms", start.elapsed().as_millis());
+        }
+        println!("Total time: {}ms", start.elapsed().as_millis());
+        results.sort();
+        println!("Results: {:?}", results);
+        println!("Total collisions: {}", results.iter().sum::<usize>());
+        println!("Median: {}", results[5]);
+        println!("Mean: {}", results.iter().sum::<usize>() / 10);
+        println!("Max: {}", results[9]);
+        println!("Min: {}", results[0]);
     }
     pub fn nickname_test() -> usize {
         let mut threads = Vec::new();
